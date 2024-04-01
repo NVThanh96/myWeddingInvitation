706ca9b2353b1354de42b864ff95cfec32e4bbab
unknown <toilatoi8624@gmail.com>
2024-04-01 14:51:56 +0700

    new code

--- template/main-content.php
+++ template/main-content.php
@@ -14,7 +14,7 @@
         </div>
 
         <h1 class="font-esthetic my-4" style="font-size: 3rem;">Văn Thành & Mai Luyến</h1>
-        <p class="mb-0" style="font-size: 1.5rem;">Thứ 3, Ngày 14 Tháng 07 Năm 2024</p>
+        <p class="mb-0" style="font-size: 1.5rem;">Chủ Nhật, Ngày 14 Tháng 07 Năm 2024</p>
 
         <a class="btn btn-outline-light btn-sm shadow rounded-pill px-3 my-2" target="_blank" href="https://calendar.google.com/calendar/u/0/r/eventedit?text=%C4%90%C3%A1m+c%C6%B0%E1%BB%9Bi+V%C4%83n+Th%C3%A0nh+%26+Mai+Luy%E1%BA%BFn&details=H%C3%B4m+nay+l%C3%A0+%C4%91%C3%A1m+c%C6%B0%E1%BB%9Bi+V%C4%83n+Th%C3%A0nh+v%C3%A0+Mai+Luy%E1%BA%BFn+%7C+Ch%E1%BB%A7+Nh%E1%BA%ADt,+Ng%C3%A0y+14+Th%C3%A1ng+07+N%C4%83m+2024+%7C+T%E1%BB%95+ch%E1%BB%A9c+t%E1%BA%A1i+nh%C3%A0+h%C3%A0ng+Ph%C3%BA+Gia+Xavia,+%C4%90%C6%B0%E1%BB%9Dng+2+Th%C3%A1ng+9,+Ho%C3%A0+C%C6%B0%E1%BB%9Dng+B%E1%BA%AFc,+H%E1%BA%A3i+Ch%C3%A2u,+%C4%90%C3%A0+N%E1%BA%B5ng+%7C+V%C3%A0o+l%C3%BAc+11h&dates=20240714T090000/20240714T100000&location=nh%C3%A0+h%C3%A0ng+Ph%C3%BA+Gia+Xavia,+%C4%90%C6%B0%E1%BB%9Dng+2+Th%C3%A1ng+9,+Ho%C3%A0+C%C6%B0%E1%BB%9Dng+B%E1%BA%AFc,+H%E1%BA%A3i+Ch%C3%A2u,+%C4%90%C3%A0+N%E1%BA%B5ng&availability=FREE">
             <i class="fa-solid fa-calendar-check me-2"></i>Lưu ngày vào lịch
